12169fc444044dfdab89e29b83869d19fc3f9364
Roman Kruk <roman1writer@gmail.com>
2018-10-16 00:57:19 +0300

    Fixed ViewModel imitation, but have data binding bug with setters and getters

--- app/src/main/java/com/eliot/ltq/databinding/MainActivity.kt
+++ app/src/main/java/com/eliot/ltq/databinding/MainActivity.kt
@@ -1,28 +1,23 @@
 package com.eliot.ltq.databinding
 
-import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.eliot.ltq.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mainViewModel : MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val mainViewModel = ViewModelProviders.of(this)
-                .get(MainViewModel::class.java)
+        mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
                 .apply { this.setLifecycleOwner(this@MainActivity)
                             this.viewmodel = mainViewModel}
 
-        mainViewModel.editTextComponent.observe( this, Observer {
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
-        })
     }
 }
--- app/src/main/java/com/eliot/ltq/databinding/MainViewModel.kt
+++ app/src/main/java/com/eliot/ltq/databinding/MainViewModel.kt
@@ -1,29 +1,14 @@
 package com.eliot.ltq.databinding
 
+import android.arch.lifecycle.ViewModel
 import android.databinding.Bindable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
+class MainViewModel : ViewModel(){
 
     val currentRandomFruitName: LiveData<String>
         get() = FakeRepository.currentRandomFruitName
 
     fun onChangeRandomFruitClick() = FakeRepository.changeCurrentRandomFruitName()
-
-    @Bindable
-    val editTextComponent = MutableLiveData<String>()
-
-    private val _displayedEditTextContent = MutableLiveData<String>()
-    val displayedEditTextContent: LiveData<String>
-        get() = _displayedEditTextContent
-
-    fun onDisplayEditTextContentClick() {
-        _displayedEditTextContent.value = editTextComponent.value
-    }
-
-    fun onSelectRandomEditTextFruit() {
-        editTextComponent.value = FakeRepository.getRandomFruitName()
-    }
 }
